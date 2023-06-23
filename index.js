311a85495f6d09b358808b8be64772a35c1207ab
AFrancisca <100122108+AFrancisca@users.noreply.github.com>
2023-06-23 16:26:05 +0200

    added loading...

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let quotes = document.getElementById('quote');
 let authors = document.getElementById('author');
 const button = document.getElementById('getQuote');
+const container = document.getElementById('container');
 
 button.addEventListener('click', randomQuote = () => {
+  button.disabled = true; // Disable the button
+  container.classList.add('loading'); // Add loading class to the container
+
   const url = "https://api.quotable.io/random";
   fetch(url)
     .then(res => {
@@ -18,10 +22,15 @@ button.addEventListener('click', randomQuote = () => {
     })
     .catch(error => {
       showError(error.message);
+    })
+    .finally(() => {
+      button.disabled = false; // Enable the button
+      container.classList.remove('loading'); // Remove loading class from the container
     });
 });
 
 const showError = (message) => {
   container.innerHTML = `<p>Error: ${message}</p>`;
 };
+
 
